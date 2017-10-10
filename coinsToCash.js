1436b7d648efccd90ef6d2704af3c515bad24b06
JessePage <jesse.page@me.com>
2017-10-10 14:05:27 -0500

    first commit

---
+++ coinsToCash.js
@@ -0,0 +1,26 @@
+// yo lets make this bitch an array of objects
+const piggyBank = { pennies: 342, nickels: 87, dimes: 45, quarters: 5 }
+let dollarAmount = 0   
+
+
+
+// blah pennies / 100
+dollarAmount += (piggyBank.pennies / 100)
+console.log(dollarAmount)
+
+//blah nickels / 20
+dollarAmount += (piggyBank.nickels / 20)
+console.log(dollarAmount)
+
+//blah dimes / 10
+dollarAmount += (piggyBank.dimes / 10)
+console.log(dollarAmount)
+
+//blah quarters / 4
+dollarAmount += piggyBank.quarters / 4
+console.log(dollarAmount)
+console.log(dollarAmount)
+
+
+ 
+
